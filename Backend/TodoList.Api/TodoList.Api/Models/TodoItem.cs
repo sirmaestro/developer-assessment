aac9eb2a75aff1b23585a14813531f0984d8c538
sirmaestro <jche681@aucklanduni.ac.nz>
2024-05-20 00:13:11 +1200

    Add simple model validations

--- Backend/TodoList.Api/TodoList.Api/Models/TodoItem.cs
+++ Backend/TodoList.Api/TodoList.Api/Models/TodoItem.cs
@@ -1,11 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoList.Api.Models
 {
     public class TodoItem
     {
+        [Required(ErrorMessage = "ID is required")]
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         public bool IsCompleted { get; set; }
